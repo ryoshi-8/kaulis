861e652271c9a7c0450dbd912fb1c26f9dcc3c39
ryoshi-8 <rsy3ryo248sa@gmail.com>
2025-04-18 17:30:19 +0900

    render-build.shの一時修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 # Categoriesテーブルの初期データ
-Category.create([{name: "キッチン"}, {name: "リビング/子ども"}, {name: "お風呂/トイレ"}])
+Category.find_or_create_by([{name: "キッチン"}, {name: "リビング/子ども"}, {name: "お風呂/トイレ"}])
 
 # Shopsテーブルの初期データ
-Shop.create([{name: "スーパー"}, {name: "ドラッグストア"}, {name: "オンラインショップ"}])
+Shop.find_or_create_by([{name: "スーパー"}, {name: "ドラッグストア"}, {name: "オンラインショップ"}])
 
 # Itemsテーブルの初期データ
 require "csv"
 
 CSV.foreach('db/items.csv', headers: true) do |row|
-  Item.create(
+  Item.find_or_create_by(
     item_name: row['item_name'],
     checked:   row['checked'],
     user_id: row['user_id'],
