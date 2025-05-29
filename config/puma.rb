2ef3c83326d416cd9bffd9ad9aab184058798914
ryoshi-8 <rsy3ryo248sa@gmail.com>
2025-05-29 17:14:56 +0900

    試しにpumaにしてみる

--- config/puma.rb
+++ config/puma.rb
@@ -1,35 +1,18 @@
-# This configuration file will be evaluated by Puma. The top-level methods that
-# are invoked here are part of Puma's configuration DSL. For more information
-# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
+# config/puma.rb
 
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
+# プロダクション環境用設定
+directory '/var/www/kaulis'
+environment 'production'
+daemonize true
 
-# Specifies that the worker count should equal the number of processors in production.
-if ENV["RAILS_ENV"] == "production"
-  require "concurrent-ruby"
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
-  workers worker_count if worker_count > 1
-end
+pidfile '/var/www/kaulis/tmp/pids/puma.pid'
+state_path '/var/www/kaulis/tmp/pids/puma.state'
+stdout_redirect '/var/www/kaulis/log/puma.stdout.log', '/var/www/kaulis/log/puma.stderr.log', true
 
-# Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+bind 'unix:///var/www/kaulis/tmp/sockets/puma.sock'
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+workers 2
+threads 1, 6
 
-# Specifies the `environment` that Puma will run in.
-environment ENV.fetch("RAILS_ENV") { "development" }
+preload_app!
 
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-
-# Allow puma to be restarted by `bin/rails restart` command.
-plugin :tmp_restart
